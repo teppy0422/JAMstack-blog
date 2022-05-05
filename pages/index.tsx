ab031b196ec496ee806d4694de4be82c92efccef
teppy0422 <teppy@aol.jp>
2022-05-05 19:19:47 +0900

    add categories for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,23 @@ import Link from "next/link";
 import { client } from "../libs/client";
 import { Container } from "@chakra-ui/react";
 
-export default function Home({ blog }) {
+export default function Home({ blog, category }) {
   return (
     <>
       <Header />
       <div style={{ height: "56px" }}></div>
+
       <Container>
+        <ul>
+          {category.map((category) => (
+            <li key={category.id}>
+              <Link href={`/category/${category.id}`}>
+                <a>{category.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div style={{ height: "200px" }}></div>
         <ul>
           {blog.map((blog) => (
             <li key={blog.id}>
@@ -26,10 +37,15 @@ export default function Home({ blog }) {
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
+  // カテゴリーコンテンツの取得
+  const categoryData = await client.get({ endpoint: "categories" });
 
   return {
     props: {
       blog: data.contents,
+      category: categoryData.contents,
     },
   };
 };
+
+<script>console.log(category)</script>;
