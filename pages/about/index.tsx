ee288f104aaf44f15377f2645c85e6306b3890fe
teppy0422 <teppy@aol.jp>
2022-05-13 18:55:51 +0900

    自己紹介文の追加

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,21 +1,81 @@
 import Content from "../../components/content";
 import Header from "../../components/header";
-import { Center } from "@chakra-ui/react";
-import Image from "next/image";
+import {
+  Center,
+  Image,
+  Text,
+  VStack,
+  Box,
+  Tooltip,
+  Flex,
+  Spacer,
+} from "@chakra-ui/react";
+import NextImage from "next/image";
+import styles from "../../styles/home.module.scss";
 
 export default function About() {
   return (
     <Content>
       <Header />
       <div style={{ height: "30px" }}></div>
-      <Center>
-        <Image
+      <div className={styles.me}>
+        <Flex>
+          <Box width={"75%"}>
+            <Text fontSize={[28, 32, 36, 40]} className={styles.name}>
+              Teppei Kataoka
+            </Text>
+            <Text className={styles.tool}>
+              Programmer for{" "}
+              <Tooltip label="Arduino" hasArrow placement="top" bg="tomato">
+                machines
+              </Tooltip>{" "}
+              and{" "}
+              <Tooltip
+                label="VBA JavaScript.."
+                hasArrow
+                placement="top"
+                bg="tomato"
+              >
+                software
+              </Tooltip>
+              .
+            </Text>
+            <Text className={styles.tool}>
+              I do{" "}
+              <Tooltip label="InkScape" hasArrow placement="top" bg="tomato">
+                design
+              </Tooltip>{" "}
+              sometimes.
+            </Text>
+          </Box>
+          <Center filter="auto" brightness="110%">
+            <NextImage
+              className={styles.pic}
+              src="/images/me.jpeg"
+              alt="me.jpeg"
+              objectFit="cover"
+              width={126}
+              height={126}
+            />
+          </Center>
+          <Spacer />
+        </Flex>
+        <Text className={styles.subTitle}>自己紹介</Text>
+        <Center>
+          <Text>
+            高知県出身のエンジニア。
+            自動車メーカーでワイヤーハーネスの製造/機械保全/生産計画/生産分析に従事。現場改善を繰り返す内にITや電子工学技術に興味を持つ。
+            EXCEL/ACCESSのソフトウェアからPLC/Arduinoなどのハードウェアを経験。それらをHTML/JavaScript/PHPで連携させる仕組みを構築。
+            現場の利用者と相談して更に発展させていくのが得意です。
+          </Text>
+        </Center>
+        <NextImage
           src="/images/hippo.gif"
           alt="hippo_walking"
           width={100}
           height={178}
         />
-      </Center>
+      </div>
     </Content>
   );
 }
