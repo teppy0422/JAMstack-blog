cbcf2da45142891a39e7033485d3fd200c2e76b6
teppy0422 <teppy@aol.jp>
2022-05-13 15:50:32 +0900

    test next/image

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,7 @@
 import Content from "../../components/content";
 import Header from "../../components/header";
-import { Center, Image } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -8,7 +9,12 @@ export default function About() {
       <Header />
       <div style={{ height: "30px" }}></div>
       <Center>
-        <Image src="/images/hippo.gif" alt="hippo_walking" width="100px" />
+        <Image
+          src="/images/hippo.gif"
+          alt="hippo_walking"
+          width={100}
+          height={178}
+        />
       </Center>
     </Content>
   );
