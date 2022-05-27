6e8c1ac289aec0cabab94708e0cc924ed81507f8
teppy0422 <teppy@aol.jp>
2022-05-27 14:34:38 +0900

    fixed privacy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,10 @@ import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
