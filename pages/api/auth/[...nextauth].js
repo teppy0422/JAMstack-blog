b7685f1b6d9cdaec489227946f018b5c60d50f89
teppy0422 <teppy@aol.jp>
2022-05-29 05:58:31 +0900

    add error messe

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,10 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
 
-const {GOOGLE_ID,GOOGLE_SECRET} = process.env
+const { GOOGLE_ID, GOOGLE_SECRET } = process.env;
 
-if (!GOOGLE_ID) throw new Error('You must provide GOOGLE_ID env var.');
-if (!GOOGLE_SECRET) throw new Error('You must provide GOOGLE_SECRET env var.');
+if (!GOOGLE_ID) throw new Error("You must provide GOOGLE_ID env var.");
+if (!GOOGLE_SECRET) throw new Error("You must provide GOOGLE_SECRET env var.");
 
 const setting = {
   providers: [
@@ -16,18 +16,7 @@ const setting = {
       clientSecret: GOOGLE_SECRET,
     }),
   ],
-  callbacks: {
-    async signIn(user, account, profile) {
-      if(account.providers=="google"){
-        const {accessToken,idToken} = account
-        try{
-          const response = await axios.post(
-            `${process.env.}`
-          )
-        }
-      }
-    },
-  },
+  callbacks: {},
   // ここに NEXTAUTH_SECRET を入れる?
   secret: process.env.NEXTAUTH_SECRET,
 };
