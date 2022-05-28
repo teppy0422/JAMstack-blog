6829bad5ddfd3e039f718b4dc6a40a97e1d4dca5
teppy0422 <teppy@aol.jp>
2022-05-28 10:08:23 +0900

    login test

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,7 +6,7 @@ export default function Component() {
     return (
       <>
         Signed in as {session.user.name} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <button onClick={() => signOut()}>Sign out_</button>
       </>
     );
   }
