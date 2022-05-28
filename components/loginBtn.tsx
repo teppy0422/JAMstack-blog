ad57c9fe2788824de242ff81a384b7691f383937
teppy0422 <teppy@aol.jp>
2022-05-29 06:30:43 +0900

    NEXTAUTH_SECRET

--- components/loginBtn.tsx
+++ components/loginBtn.tsx
@@ -1,25 +1,29 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Button } from "@chakra-ui/react";
-import styles from "../styles/home.module.scss";
+import { Button, Box } from "@chakra-ui/react";
 
 export default function Component() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <div id="login" style={{ display: "none" }}>
-        Signed in as {session.user.email} <br />
-        <Button colorScheme="pink" h={7} onClick={() => signOut()}>
-          Sign out
-        </Button>
-      </div>
-    );
-  }
   return (
-    <div id="login" style={{ display: "none" }}>
-      Not signed in <br />
-      <Button colorScheme="pink" h={7} onClick={() => signIn()}>
-        Sign in
-      </Button>
-    </div>
+    <>
+      {session ? (
+        <Button
+          colorScheme="blue"
+          onClick={() => signOut()}
+          id="login"
+          style={{ display: "none" }}
+        >
+          {session.user.name}:Sign out
+        </Button>
+      ) : (
+        <Button
+          colorScheme="blue"
+          onClick={() => signIn()}
+          id="login"
+          style={{ display: "none" }}
+        >
+          Sign in
+        </Button>
+      )}
+    </>
   );
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,24 +1,19 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Button } from "@chakra-ui/react";
+import { Button, Box } from "@chakra-ui/react";
 
 export default function Component() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.name} <br />
-        <Button colorScheme="blue" onClick={() => signOut()}>
-          Sign out_
-        </Button>
-      </>
-    );
-  }
   return (
     <>
-      Not signed in <br />
-      <Button colorScheme="blue" onClick={() => signIn()}>
-        Sign in
-      </Button>
+      {session ? (
+        <Button colorScheme="blue" onClick={() => signOut()}>
+          Sign out_ {session.user.name}
+        </Button>
+      ) : (
+        <Button colorScheme="blue" onClick={() => signIn()}>
+          Sign in
+        </Button>
+      )}
     </>
   );
 }
