3b9383b43be1f8fa80a109d93e3d39fd4bfd4686
teppy0422 <teppy@aol.jp>
2022-06-22 05:16:36 +0900

    タイプミスがあった場合の復帰に問題

--- pages/app/typing copy.jsx
+++ pages/app/typing copy.jsx
@@ -7,15 +7,9 @@ import ResponseCache from "next/dist/server/response-cache";
 import { type } from "os";
 import { now } from "lodash";
 
-import { romaji } from "../../libs/romaji.js";
-
 const typing = () => {
   const [inputText, setInputText] = useState("");
-  const [inputTextTemp, setInputTextTemp] = useState("");
-  const [Q_Text, setQ_Text] = useState("");
-  const [correctCount, setCorrectCount] = useState(0);
   const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
-  let inputTextTempA = "";
   // 非同期処理
   function GetRandomSentence() {
     return fetch(RANDOM_SENTENCE_URL_API)
@@ -23,66 +17,15 @@ const typing = () => {
       .then((data) => data.content);
   }
 
-  //入力毎のイベント
+  // inputテキスト入力が合っているかどうかの判定
   useEffect(() => {
+    // console.log(inputText);
     const sentenceArray = document
-      .getElementById("type-display-hiragana")
+      .getElementById("type-display")
       .querySelectorAll("span");
-
-    console.log({ inputText });
-
-    inputTextTempA = inputTextTempA + inputText.charAt(inputText.length - 1);
-
-    console.log({ inputTextTempA });
-
-    var temp = romaji(inputText);
-
-    console.log({ temp });
-
-    switch (temp) {
-      // 初回
-      case "":
-        console.log("");
-        break;
-      //入力候補がない場合
-      case "notMore":
-        console.log("notMore");
-        document.getElementById("type-input").value = "";
-        break;
-      //入力候補がある場合
-      case "More":
-        console.log("More");
-        break;
-      //マッチした場合
-      default:
-        console.log("default");
-        // 入力文字が正しい場合
-        if (Q_Text.substr(0, temp.length) === temp) {
-          setQ_Text(Q_Text.substr(temp.length));
-          setCorrectCount(correctCount + temp.length);
-          document.getElementById("type-input").value = "";
-          sentenceArray.forEach((spans, index) => {
-            // console.log(index);
-            if (index <= correctCount + temp.length - 1) {
-              spans.style.color = "red";
-            }
-          });
-        }
-        break;
-    }
-    console.log("Q_Text: " + Q_Text.substr(temp.length));
-    console.log("correctCount: " + correctCount + temp.length);
-
-    // 処理を中断
-    const test = true;
-    if (test) {
-      return;
-    }
-
-    console.log("return");
-
+    // console.log(sentenceArray);
     const inputTextSp = inputText.split("");
-    console.log(inputTextSp);
+    // console.log(inputTextSp);
 
     sentenceArray.forEach((spans, index) => {
       const inputTextTemp = inputTextSp[index];
@@ -92,13 +35,11 @@ const typing = () => {
           inputTextTemp = inputTextTemp.toUpperCase();
         }
 
-        // console.log(inputTextTemp);
+        console.log(inputTextTemp);
 
         if (correctText == inputTextTemp) {
           console.log("correct");
           spans.style.color = "red";
-          count = count + 1;
-          console.log({ count });
         } else {
           spans.style.color = "black";
         }
@@ -109,11 +50,34 @@ const typing = () => {
   // ランダムな文章を取得して表示
   async function RenderNextSentence() {
     const typeDisplay = document.getElementById("type-display");
-    const typeDisplayHiragana = document.getElementById(
-      "type-display-hiragana"
-    );
     const typeInput = document.getElementById("type-input");
-    // console.log(typeInput);
+    console.log(typeInput);
+
+    const sentence = await GetRandomSentence();
+    console.log(sentence);
+    typeDisplay.innerText = sentence;
+    // 文章を一文字ずつ分解してspanタグを生成
+    let oneText = sentence.split("");
+    // console.log(oneText);
+    typeDisplay.innerText = "";
+    oneText.forEach((character) => {
+      const characterSpan = document.createElement("span");
+      characterSpan.innerText = character;
+      console.log(characterSpan);
+      typeDisplay.appendChild(characterSpan);
+      // characterSpan.classList.add("correct");
+    });
+    // テキストボックスの値を削除
+    typeInput.value = "";
+
+    StartTimer();
+  }
+
+  // ランダムな文章を取得して表示2
+  async function RenderNextSentence2() {
+    const typeDisplay = document.getElementById("type-display");
+    const typeInput = document.getElementById("type-input");
+    console.log(typeInput);
 
     const Q = [
       [
@@ -171,35 +135,25 @@ const typing = () => {
       ["やってはいけないことをやってみる", "やってはいけないことをやってみる"],
       [
         "中途半端にやると他人のマネになる。とことんやると他人がマネできないものになる",
-        "ちゅうとはんぱにやるとたにんのまねになる。とこんやるとたにんがまねできないものになる",
+        "ちゅうとはんぱにやるとたにんのまねになる。ところんやるとたにんがまねできないものになる",
       ],
     ];
-    // console.log(Q);
+    console.log(Q);
     let Q_No = Math.floor(Math.random() * Q.length); //問題をランダムで出題する
 
-    // console.log(Q[Q_No][0]);
-    // console.log(Q[Q_No][1]);
+    console.log(Q[Q_No][0]);
+    console.log(Q[Q_No][1]);
 
     typeDisplay.innerText = "";
-    typeDisplayHiragana.innerText = "";
     // 文章を一文字ずつ分解してspanタグを生成
     let oneText = Q[Q_No][0].split("");
+    console.log(oneText);
     oneText.forEach((character) => {
       const characterSpan = document.createElement("span");
       characterSpan.innerText = character;
-      // console.log(characterSpan);
+      console.log(characterSpan);
       typeDisplay.appendChild(characterSpan);
     });
-    // 文章を一文字ずつ分解してspanタグを生成_ひらがな
-    let oneTextHiragana = Q[Q_No][1].split("");
-    oneTextHiragana.forEach((character) => {
-      const characterSpan = document.createElement("span");
-      characterSpan.innerText = character;
-      // console.log(characterSpan);
-      typeDisplayHiragana.appendChild(characterSpan);
-    });
-    // 問題のセット
-    setQ_Text(Q[Q_No][1]);
     // テキストボックスの値を削除
     typeInput.value = "";
 
@@ -207,7 +161,7 @@ const typing = () => {
   }
 
   let startTime;
-  let originTime = 300;
+  let originTime = 10;
   function StartTimer() {
     const timer = document.getElementById("timer");
     timer.innerText = originTime;
@@ -220,7 +174,7 @@ const typing = () => {
   }
 
   function TimeUp() {
-    RenderNextSentence();
+    RenderNextSentence2();
     StartTimer();
   }
 
@@ -230,7 +184,7 @@ const typing = () => {
 
   //マウント時に一回だけ実行
   useEffect(() => {
-    RenderNextSentence();
+    RenderNextSentence2();
     document.getElementById("type-input").focus();
   }, []);
 
@@ -242,10 +196,6 @@ const typing = () => {
         </div>
         <Box className={styles.container}>
           <div className={styles.typeDisplay} id="type-display"></div>
-          <div
-            className={styles.typeDisplayHiragana}
-            id="type-display-hiragana"
-          ></div>
           <textarea
             className={styles.typeInput}
             id="type-input"
