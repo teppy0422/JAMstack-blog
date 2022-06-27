85419ef276cd5b4720651362cbe199c36d6630cd
teppy0422 <teppy@aol.jp>
2022-06-28 01:57:21 +0900

    test magic.link

--- pages/login2.tsx
+++ pages/login2.tsx
@@ -4,8 +4,11 @@ import { Button, Box } from "@chakra-ui/react";
 import Content from "../components/content";
 
 import { Magic } from "magic-sdk";
+import { OAuthExtension } from "@magic-ext/oauth";
 
 let magicEmail: any;
+let magicGoogle: any;
+let magicGithub: any;
 
 const App = () => {
   const emailRef = useRef<HTMLInputElement>(null);
@@ -13,6 +16,12 @@ const App = () => {
 
   useEffect(() => {
     magicEmail = new Magic("pk_live_6F968428E7D10FDA");
+    magicGoogle = new Magic("pk_live_6F968428E7D10FDA", {
+      extensions: [new OAuthExtension()],
+    });
+    const magicGithub = new Magic("pk_live_6F968428E7D10FDA", {
+      extensions: [new OAuthExtension()],
+    });
   }, []);
 
   async function handleLogin() {
@@ -25,6 +34,29 @@ const App = () => {
       //リダイレクト
       router.replace("./login");
     } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleLoginWithGoogle() {
+    try {
+      await magicGoogle.oauth.loginWithRedirect({
+        provider: "google",
+        redirectURI: "http://localhost:3000/login",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleGoginWithGithub() {
+    try {
+      await magicGithub.oauth.loginWithRedirect({
+        provider: "github",
+        redirectURI: "https://www.teppy.link/",
+      });
+    } catch (err) {
+      console.log("err:");
       console.log(err);
     }
   }
@@ -43,7 +75,10 @@ const App = () => {
               <Button>Twitter</Button>
               <br />
               <div style={{ height: "10px" }} />
-              <Button>Github</Button>
+              <Button onClick={() => handleLoginWithGoogle()}>Google</Button>
+              <br />
+              <div style={{ height: "10px" }} />
+              <Button onClick={() => handleGoginWithGithub()}>Github</Button>
             </div>
           </div>
         </div>
