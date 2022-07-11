d350d2d6ddfd1020b8c569b6cbe01734a826ce9d
teppy0422 <teppy@aol.jp>
2022-07-11 09:36:13 +0900

    missedの追加

--- pages/api/typing.ts
+++ pages/api/typing.ts
@@ -4,7 +4,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<TypingResult[] | TypingResult>
+) {
   const { method } = req;
   console.log({ method });
 
