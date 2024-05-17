fb9fb9c7136db4ce534594dd59ef71851ee0cedf
teppy0422 <teppy@aol.jp>
2024-05-18 02:22:03 +0900

    sjpのダウンロードを追加

--- pages/download.tsx
+++ pages/download.tsx
@@ -6,7 +6,12 @@ const DownloadPage = () => {
     <div>
       <h1>ダウンロード</h1>
       <Link href="/files/downloadTxt.txt" passHref>
-        <a download="downloadTxt.txt">こちらからダウンロード</a>
+        <a download="downloadTxt.txt">こちらからダウンロードaaa</a>
+      </Link>
+      <Link href="/files/Sjp3.004.62_464D_82161-6BN31㈹aテスト.zip" passHref>
+        <a download="Sjp3.004.62_464D_82161-6BN31㈹aテスト.zip">
+          こちらからダウンロードaaa
+        </a>
       </Link>
     </div>
   );
