698ea039143fceafcdcac4d5550d743d4f75af60
teppy0422 <teppy@aol.jp>
2024-05-19 09:59:57 +0900

    typescriptのバージョンを戻す前

---
+++ components/popver.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  Button,
+} from "@chakra-ui/react";
+
+const CustomPopover = ({ isOpen, onOpen, onClose }) => (
+  <Popover isOpen={isOpen} onClose={onClose}>
+    <PopoverTrigger>
+      <Button onClick={onOpen} _focus="none">
+        Trigger
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent _focus="none">
+      <PopoverArrow />
+      <PopoverCloseButton />
+      <PopoverHeader>Download</PopoverHeader>
+      <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+    </PopoverContent>
+  </Popover>
+);
+
+export default CustomPopover;
