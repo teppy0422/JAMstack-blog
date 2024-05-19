7ef912f77a437ffc3622664a4488370c41709732
teppy0422 <teppy@aol.jp>
2024-05-19 14:27:32 +0900

    addDownloadSite+Popover

--- components/customLinkBox.tsx
+++ components/customLinkBox.tsx
@@ -1,42 +1,73 @@
 import React from "react";
-import { LinkBox, Box, Text, Heading, LinkOverlay } from "@chakra-ui/react";
-
+import {
+  LinkBox,
+  Box,
+  Text,
+  Heading,
+  LinkOverlay,
+  PopoverFooter,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  Button,
+  Link,
+} from "@chakra-ui/react";
 type CustomLinkBoxProps = {
   dateTime: string;
-  daysAgo: string;
-  heading: string;
   description: string;
   linkHref: string;
-  linkText: string;
+  ver: string;
 };
-
+// elapsedHoursを画面に表示する処理を追加
 class CustomLinkBox extends React.Component<CustomLinkBoxProps> {
   render() {
+    const elapsedHours = Math.floor(
+      (new Date().getTime() - new Date(this.props.dateTime).getTime()) /
+        (1000 * 60 * 60)
+    );
+    const daysAgo = `${elapsedHours / 24} days ago`;
     return (
-      <LinkBox
-        as="article"
-        maxW="sm"
-        p="4"
-        borderWidth="1px"
-        rounded="md"
-        borderColor="gray.500"
-      >
-        <Box as="time" dateTime={this.props.dateTime}>
-          {this.props.daysAgo}
-        </Box>
-        <Heading size="md" my="2">
-          <LinkOverlay href="#">{this.props.heading}</LinkOverlay>
-        </Heading>
-        <Text mb="3">{this.props.description}</Text>
-        <Box
-          as="a"
-          color="teal.400"
-          href={this.props.linkHref}
-          fontWeight="bold"
-        >
-          {this.props.linkText}
-        </Box>
-      </LinkBox>
+      <Popover>
+        <PopoverTrigger>
+          <LinkBox
+            as="article"
+            maxW="sm"
+            p="3"
+            borderWidth="1px"
+            rounded="md"
+            borderColor="gray.500"
+          >
+            <Box as="time" dateTime={this.props.dateTime} fontSize="sm">
+              {daysAgo}
+            </Box>
+            <Heading size="md" my="2">
+              <LinkOverlay href="#">{this.props.ver}</LinkOverlay>
+            </Heading>
+            <Text mb="3">{this.props.description}</Text>
+          </LinkBox>
+        </PopoverTrigger>
+        <PopoverContent _focus={{ _focus: "none" }}>
+          <PopoverArrow bg="green.500" />
+          <PopoverCloseButton />
+          <PopoverHeader bg="green.500" borderTopRadius="md">
+            {this.props.ver}
+          </PopoverHeader>
+          <PopoverBody>
+            ダウンロードファイルを開いてアップロードを実行してください。これはテストです。不具合がある場合は連絡ください。
+          </PopoverBody>
+          <PopoverFooter>
+            <Link href={this.props.linkHref} _focus={{ _focus: "none" }}>
+              <Button>
+                <a download="Sjp3.004.99_.xlsm">Download</a>
+              </Button>
+            </Link>
+          </PopoverFooter>
+        </PopoverContent>
+      </Popover>
     );
   }
 }
--- pages/download/index.tsx
+++ pages/download/index.tsx
@@ -33,22 +33,11 @@ import NextImage from "next/image";
 import styles from "../../styles/home.module.scss";
 
 import Hippo_001_wrap from "../../components/3d/hippo_001_wrap";
-import Alagin_wrap from "../../components/3d/alagin_wrap";
-import Table_wrap from "../../components/3d/table_wrap";
-import Speaker_wrap from "../../components/3d/speaker_wrap";
-import Sushi_ebi_wrap from "../../components/3d/sushi_ebi_wrap";
-import Sushi_ika_wrap from "../../components/3d/sushi_ika_wrap";
-import Sushi_ootoro_wrap from "../../components/3d/sushi_ootoro_wrap";
-import Sushi_tamago_wrap from "../../components/3d/sushi_tamago_wrap";
-import Sushi_ikura_wrap from "../../components/3d/sushi_ikura_wrap";
 
 import CustomLinkBox from "../../components/customLinkBox";
 import CustomPopver from "../../components/popver";
 export default function About() {
-  const illusts = [
-    { src: "/images/illust/hippo/hippo_001.png" },
-    { src: "/images/illust/hippo/hippo_001_a.png" },
-  ];
+  const illusts = [{ src: "/images/illust/hippo/hippo_001.png" }];
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   return (
@@ -69,8 +58,8 @@ export default function About() {
           <Flex>
             <Box mr={3}>
               <Box className={styles.watercolor} h={100}>
-                <Text p={5} fontSize="32px" fontWeight="700">
-                  イラスト
+                <Text p={6} fontSize="32px" fontWeight="700">
+                  ダウンロード
                 </Text>
               </Box>
               <Text className={styles.tool} pl={5}>
@@ -91,50 +80,20 @@ export default function About() {
           </Flex>
         </VStack>
 
+        <Text>aaa</Text>
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
           <CustomLinkBox
-            dateTime="2021-01-15T15:30:00+0000"
-            daysAgo="2 days ago"
-            heading="3.004.64"
+            dateTime="2024-05-19T13:30:00+0900"
+            description="先ハメのみでも空詮表示をする"
+            linkHref="/files/Sjp3.004.63_.xlsm"
+            ver="3.004.63"
+          />
+          <CustomLinkBox
+            dateTime="2024-05-18T02:21:00+0900"
             description="MenuからWEBサイトにアクセスするアドレスの修正"
-            linkHref="#inner-link"
-            linkText="Download"
-            onBoxClick={() => setIsOpen(true)} // CustomLinkBoxのクリックでPopoverを開く
+            linkHref="/files/Sjp3.004.62_.xlsm"
+            ver="3.004.62"
           />
-          <CustomLinkBox
-            dateTime="2021-01-15 15:30:00 +0000 UTC"
-            daysAgo="2 days ago"
-            heading="3.004.63"
-            description="何か"
-            linkHref="#inner-link"
-            linkText="Download"
-          />
-          <CustomLinkBox
-            dateTime="2021-01-15 15:30:00 +0000 UTC"
-            daysAgo="2 days ago"
-            heading="3.004.63"
-            description="何か"
-            linkHref="#inner-link"
-            linkText="Download"
-          />
-          <CustomLinkBox
-            dateTime="2021-01-15 15:30:00 +0000 UTC"
-            daysAgo="2 days ago"
-            heading="3.004.63"
-            description="何か"
-            linkHref="#inner-link"
-            linkText="Download"
-          />
-          <CustomLinkBox
-            dateTime="2021-01-15 15:30:00 +0000 UTC"
-            daysAgo="2 days ago"
-            heading="3.004.63"
-            description="何か"
-            linkHref="#inner-link"
-            linkText="Download"
-          />
-
-          <CustomPopver />
         </SimpleGrid>
 
         <Box ml={[0, 18, 70, 115]}>
@@ -142,7 +101,6 @@ export default function About() {
             <Text className={styles.subTitle}>サンプル</Text>
           </div>
         </Box>
-
         {/* <Sample3d /> */}
 
         <Box style={{ textAlign: "center" }}>
@@ -168,27 +126,6 @@ export default function About() {
             );
           })}
           <Hippo_001_wrap />
-          <Alagin_wrap />
-          <Table_wrap />
-          <Speaker_wrap />
-          <Sushi_ebi_wrap />
-          <Sushi_ika_wrap />
-          <Sushi_ootoro_wrap />
-          <Sushi_tamago_wrap />
-          <Sushi_ikura_wrap />
-        </Box>
-        <Box mt={10} textAlign="center">
-          <Link href="/files/downloadTxt.txt" passHref>
-            <a download="downloadTxt.txt">こちらからダウンロードaaa</a>
-          </Link>
-          <Link
-            href="/files/Sjp3.004.62_464D_82161-6BN31㈹aテスト.zip"
-            passHref
-          >
-            <a download="Sjp3.004.62_464D_82161-6BN31㈹aテスト.zip">
-              こちらからダウンロードaaa
-            </a>
-          </Link>
         </Box>
       </div>
     </Content>
