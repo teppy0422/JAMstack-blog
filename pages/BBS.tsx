92623c2c86103243afd2286de92ea3eaee8b50ac
teppy0422 <teppy@aol.jp>
2024-06-27 18:58:42 +0900

    addBBS_20240627.1833

--- pages/BBS.tsx
+++ pages/BBS.tsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from "react";
 
-export default async function HOME() {
-  const response = await fetch("http://localhost:3000/api/BBSpost", {
-    cache: "no-store",
-  });
-  const bbsAllData = await response.json();
-  console.log(bbsAllData);
+export default function HOME() {
+  const [bbsData, setBbsData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/api/BBSpost", {
+        cache: "no-store",
+      });
+      const data = await response.json();
+      setBbsData(data);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>掲示板</h1>
+      {bbsData && <div>{JSON.stringify(bbsData)}</div>}
     </div>
   );
 }
