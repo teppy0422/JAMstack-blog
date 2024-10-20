7bfb550d8d8824586d6a377585cf1f4263c83acf
teppy0422 <teppy@aol.jp>
2024-10-21 03:52:55 +0900

    add3.100.33

--- pages/download/sjp.tsx
+++ pages/download/sjp.tsx
@@ -144,13 +144,22 @@ export default function About() {
             mx={{ base: 2, md: 20, lg: 40, xl: 50 }}
           >
             <CustomLinkBox
+              dateTime="2024-10-21T03:48:00+0900"
+              description1=""
+              description2="オートシェイプを.pngで出力するsampleを追加"
+              descriptionIN=""
+              linkHref="/files/download/Sjp/Sjp3.100.33_.zip"
+              inCharge="Win10zip"
+              isLatest={true}
+            />
+            <CustomLinkBox
               dateTime="2024-10-18T16:11:00+0900"
               description1="部品リストIEで略図ダウンロード時にエラー"
               description2="略図保存pathをVer3系に修正"
               descriptionIN=""
               linkHref="/files/download/Sjp/Sjp3.100.32_.zip"
               inCharge="高知,王さん,Win10zip"
-              isLatest={true}
+              isLatest={false}
             />
             <CustomLinkBox
               dateTime="2024-10-18T14:11:00+0900"
