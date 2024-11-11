45a523697b8377cf04371c20385296891b726383
teppy0422 <teppy@aol.jp>
2024-11-11 19:51:16 +0900

    add3.100.73

--- pages/download/sjp.tsx
+++ pages/download/sjp.tsx
@@ -144,13 +144,22 @@ export default function About() {
             mx={{ base: 2, md: 20, lg: 40, xl: 50 }}
           >
             <CustomLinkBox
+              dateTime="2024-11-11T19:50:00+0900"
+              description1="後ハメのマジックが表示されない"
+              description2="3.100.56の修正を更に修正"
+              descriptionIN=""
+              linkHref="/files/download/Sjp/Sjp3.100.73_.zip"
+              inCharge="高知,王さん"
+              isLatest={true}
+            />
+            <CustomLinkBox
               dateTime="2024-11-10T21:04:00+0900"
               description1="サブ図で同端末/異なる部品品番がある場合に正しくない"
               description2="端末サブナンバー取得時に端末と部品品番で照合するように修正"
               descriptionIN=""
               linkHref="/files/download/Sjp/Sjp3.100.72_.zip"
               inCharge="高知,王さん"
-              isLatest={true}
+              isLatest={false}
             />
             <CustomLinkBox
               dateTime="2024-11-08T15:45:00+0900"
